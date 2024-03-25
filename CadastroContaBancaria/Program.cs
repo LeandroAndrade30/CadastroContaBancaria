3c4b954163af19caa2b93868e380bbd099ba1305
Leandro Andrade <leandrade666@gmail.com>
2024-03-25 16:25:46 -0300

    Criado Projeto CadastroContaBancaria

---
+++ CadastroContaBancaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadastroContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
