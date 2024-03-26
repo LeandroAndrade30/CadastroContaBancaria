596defcbfd2418dd8b2c89cfa48e8c9ebf769d29
Leandro Andrade <leandrade666@gmail.com>
2024-03-26 15:52:39 -0300

    Criado versão do projeto CadastroContaBancaria com encapsulamento

--- CadastroContaBancaria/ContaBancaria.cs
+++ CadastroContaBancaria/ContaBancaria.cs
@@ -6,5 +6,80 @@ namespace CadastroContaBancaria
 {
     class ContaBancaria
     {
+        private int _numeroConta;
+        private string _nomeTitular;
+        private double _saldo;
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial = 0.0)
+        {
+            _numeroConta = numeroConta;
+            _nomeTitular = nomeTitular;
+            _saldo = depositoInicial;
+
+        }
+
+        public int NumeroConta
+        {
+            get
+            {
+                return _numeroConta;
+            }
+        }
+
+        public string NomeTitular
+        {
+            get
+            {
+                return _nomeTitular;
+            }
+
+
+            set
+            {
+                _nomeTitular = value;
+            }
+        }
+
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+        }
+
+        public void DepositoInicial(double valor)
+        {
+
+            _saldo = _saldo + valor;
+
+            Console.WriteLine($"\n Depósito de R$ {valor} Realizado com sucesso!");
+        }
+
+        public void Saque(double valor)
+        {
+            if (_saldo >= valor)
+            {
+                _saldo = _saldo - valor;
+                _saldo = _saldo - 5;
+
+                Console.WriteLine($"\n Saque de R$ {valor} Realizado com sucesso! Seu saldo atual é de R$ {_saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Seu saldo é insuficiente para realizar o saque.");
+            }
+        }
+
+        public void ExibirDadosBancarios()
+        {
+            Console.WriteLine("\n Dados da Conta: ");
+            Console.WriteLine($"Número da conta: {NumeroConta}");
+            Console.WriteLine($"Nome do Titular: {NomeTitular}");
+            Console.WriteLine($"Saldo $ {Saldo.ToString("F2")}");
+        }
+
+
     }
 }
--- CadastroContaBancaria/Program.cs
+++ CadastroContaBancaria/Program.cs
@@ -6,7 +6,50 @@ namespace CadastroContaBancaria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o nome do titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.WriteLine("Deseja realizar um depósito inicial na sua conta?(s/n)");
+            char resposta = char.Parse(Console.ReadLine());
+
+            ContaBancaria ContaBancaria;
+
+            if (resposta.ToString().ToLower() == "s")
+            {
+                Console.WriteLine("Informe o valor do depósito inicial: R$ ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+
+                ContaBancaria = new ContaBancaria(numeroConta, nomeTitular, depositoInicial);
+
+            }
+            else
+            {
+                ContaBancaria = new ContaBancaria(numeroConta, nomeTitular);
+            }
+
+
+
+            ContaBancaria.ExibirDadosBancarios();
+
+            Console.WriteLine("Digite o valor do depósito");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            ContaBancaria.DepositoInicial(valorDeposito);
+
+
+            ContaBancaria.ExibirDadosBancarios();
+            
+
+
+            
+            Console.WriteLine($"Informe o valor do saque: R$  ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            ContaBancaria.Saque(valorSaque);
+
+
+
+
+
         }
     }
 }
